9a7406599b017420947f458c63167a7c6492002d
Eric Tobin <eatobin@gmail.com>
2026-01-17 22:21:23 -0700

    progress

--- day01/test/Spec.hs
+++ day01/test/Spec.hs
@@ -1,16 +1,8 @@
 -- file Spec.hs From Eric!
+import Lib (someFunc)
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
-
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
-
-    it "throws an exception if used with an empty list" $ do
-      evaluate (head []) `shouldThrow` anyException
+    it "works" $ do
+      someFunc `shouldBe` (101 :: Int)
