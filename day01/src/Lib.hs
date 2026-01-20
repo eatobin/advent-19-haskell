83f1aaf9a82aa6ed1943e2b18e5c9baa6be8b326
eatobin <eatobin@users.noreply.github.com>
2026-01-19 22:21:34 -0500

    infix

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -3,7 +3,7 @@ module Lib (someFunc, gas) where
 import Numeric.Natural (Natural)
 
 someFunc :: Natural -> Natural
-someFunc x = (+) x 1
+someFunc x = x + 1
 
 gas :: Natural -> Natural
-gas myModule = (-) (div myModule 3) 2
+gas myModule = (div myModule 3) - 2
