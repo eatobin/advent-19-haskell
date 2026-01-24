e352252594b013f7511785ad02947bd521c2e429
Eric Tobin <eatobin@gmail.com>
2026-01-23 22:44:10 -0700

    sumGas

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (someFunc, mySomeFunc, gas) where
+module Lib (someFunc, mySomeFunc, gas, sumGas) where
 
 import Numeric.Natural (Natural)
 
@@ -10,3 +10,6 @@ mySomeFunc x = x + 1
 
 gas :: Natural -> Natural
 gas myModule = div myModule 3 - 2
+
+sumGas :: [Natural] -> Natural
+sumGas = sum . map gas
