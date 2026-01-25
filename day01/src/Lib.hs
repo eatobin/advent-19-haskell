ab04efbc4e627c217652dd642b6a0aa4e1f05af0
Eric Tobin <eatobin@gmail.com>
2026-01-24 23:42:01 -0700

    start part B

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (gasA, sumGasA) where
+module Lib (gasA, gasB, sumGasA, sumGasB) where
 
 import Numeric.Natural (Natural)
 
@@ -7,3 +7,9 @@ gasA myModule = div myModule 3 - 2
 
 sumGasA :: [Natural] -> Natural
 sumGasA = sum . map gasA
+
+gasB :: Int -> Int
+gasB myModule = div myModule 3 - 2
+
+sumGasB :: [Int] -> Int
+sumGasB = sum . map gasB
