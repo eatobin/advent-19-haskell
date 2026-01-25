85d0aea6d6a4dcc0fa86724430452b68fcf67c4f
Eric Tobin <eatobin@gmail.com>
2026-01-24 21:11:23 -0700

    gasA and sumGasA

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -1,9 +1,9 @@
-module Lib (gas, sumGas) where
+module Lib (gasA, sumGasA) where
 
 import Numeric.Natural (Natural)
 
-gas :: Natural -> Natural
-gas myModule = div myModule 3 - 2
+gasA :: Natural -> Natural
+gasA myModule = div myModule 3 - 2
 
-sumGas :: [Natural] -> Natural
-sumGas = sum . map gas
+sumGasA :: [Natural] -> Natural
+sumGasA = sum . map gasA
--- day01/test/Spec.hs
+++ day01/test/Spec.hs
@@ -1,5 +1,5 @@
 import Control.Exception (evaluate)
-import Lib (gas)
+import Lib (gasA)
 import Numeric.Natural (Natural)
 import Test.Hspec (anyException, hspec, it, shouldBe, shouldThrow)
 
@@ -7,19 +7,19 @@ main :: IO ()
 main = hspec $ do
   it "all by itself" $ do
     (78 :: Natural) `shouldBe` (78 :: Natural)
-  it "gas at 0 should fail" $ do
-    evaluate (gas 0) `shouldThrow` anyException
-  it "gas at 3 should fail" $ do
-    evaluate (gas 3) `shouldThrow` anyException
-  it "gas at 6 should pass with zero" $ do
-    gas 6 `shouldBe` 0
-  it "gas at 9" $ do
-    gas 9 `shouldBe` 1
-  it "gas at 12" $ do
-    gas 12 `shouldBe` 2
-  it "gas at 14" $ do
-    gas 14 `shouldBe` 2
-  it "gas at 1969" $ do
-    gas 1969 `shouldBe` 654
-  it "gas at 100756" $ do
-    gas 100756 `shouldBe` 33583
+  it "gasA at 0 should fail" $ do
+    evaluate (gasA 0) `shouldThrow` anyException
+  it "gasA at 3 should fail" $ do
+    evaluate (gasA 3) `shouldThrow` anyException
+  it "gasA at 6 should pass with zero" $ do
+    gasA 6 `shouldBe` 0
+  it "gasA at 9" $ do
+    gasA 9 `shouldBe` 1
+  it "gasA at 12" $ do
+    gasA 12 `shouldBe` 2
+  it "gasA at 14" $ do
+    gasA 14 `shouldBe` 2
+  it "gasA at 1969" $ do
+    gasA 1969 `shouldBe` 654
+  it "gasA at 100756" $ do
+    gasA 100756 `shouldBe` 33583
