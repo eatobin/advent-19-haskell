58e0f7912a18afe73dc1d2d7ea5a300907743b5b
eatobin <eatobin@users.noreply.github.com>
2026-01-25 17:56:04 -0700

    add example

--- day01/src/Lib.hs
+++ day01/src/Lib.hs
@@ -13,3 +13,14 @@ gasB myModule = div (fromIntegral myModule) 3 - 2
 
 sumGasB :: [Natural] -> Int
 sumGasB = sum . map gasB
+
+
+-- The top-level function signature
+myLength :: [a] -> Int
+myLength xs = go xs 0  -- Calls the inner helper 'go'
+
+  where
+    -- The inner, tail-recursive helper function
+    go :: [a] -> Int -> Int
+    go [] n     = n          -- Base case: return the accumulator when the list is empty
+    go (_:xs) n = go xs (n + 1) -- Recursive case: increment accumulator and recurse on the tail
