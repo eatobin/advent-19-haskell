e406a6c1538c5fd56d90cdf73e33a71c2fdfccea
Eric Tobin <eatobin@gmail.com>
2026-01-30 11:16:38 -0700

    begin record

--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -2,3 +2,10 @@ module Lib (someFunc) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Case =
+  Case
+    { description :: String,
+      input :: Integer,
+      expected :: Bool
+    }
