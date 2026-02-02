2ba8cfa2b6ae76007cd60a3404cc233c1eeb76fb
Eric Tobin <eatobin@gmail.com>
2026-02-02 13:09:21 -0700

    progress

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
-import Lib
+import Lib (case1, lookupResult1, lookupResult2, lookupResult3, updatedMap)
 
 main :: IO ()
-main = someFunc
+main =
+  do
+    print lookupResult1
+    print lookupResult2
+    print updatedMap
+    print lookupResult3
+    print case1
--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -1,15 +1,33 @@
-module Lib (someFunc) where
+module Lib (lookupResult1, lookupResult2, updatedMap, lookupResult3, case1) where
 
-someFunc :: IO ()
-someFunc = print case1
+import qualified Data.Map as M
+import qualified Data.Maybe as DM
 
-data Case =
-  Case
-    { description :: String,
-      input :: Integer,
-      expected :: Bool
-    }
-  deriving Show
+hasRedHat :: M.Map Int Int
+hasRedHat = M.fromList [(0, 0), (1, 11), (2, 22)]
+
+-- Look up an existing key
+lookupResult1 :: Maybe Int
+lookupResult1 = M.lookup 0 hasRedHat -- Just True
+
+-- Look up a non-existing key
+lookupResult2 :: Maybe Int
+lookupResult2 = M.lookup 19 hasRedHat -- Nothing
+
+updatedMap :: M.Map Int Int
+updatedMap = M.insert 19 99 hasRedHat
+
+-- Look up a non-existing key
+lookupResult3 :: Int
+lookupResult3 = DM.fromJust (M.lookup 19 updatedMap) -- Nothing
+
+data Case
+  = Case
+  { description :: String,
+    input :: Integer,
+    expected :: Bool
+  }
+  deriving (Show)
 
 case1 :: Case
-case1 = Case { description = "456 Oak Ave", input = 42, expected = True }
+case1 = Case {description = "456 Oak Ave", input = 42, expected = True}
