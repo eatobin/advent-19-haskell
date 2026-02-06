201395160d66f59b64b9c927f148305ddee0e811
Eric Tobin <eatobin@gmail.com>
2026-02-05 17:29:08 -0700

    correction and progress

--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -1,4 +1,4 @@
-module Lib (lookupResult1, lookupResult2, updatedMap, lookupResult3, case1) where
+module Lib (lookupResult1, lookupResult2, updatedMap, lookupResult3, case1, goodResult, badResult, vvv) where
 
 import qualified Data.Map as M
 import qualified Data.Maybe as DM
@@ -17,7 +17,6 @@ lookupResult2 = M.lookup 19 hasRedHat -- Nothing
 updatedMap :: M.Map Int Int
 updatedMap = M.insert 19 99 hasRedHat
 
--- Look up a non-existing key
 lookupResult3 :: Int
 lookupResult3 = DM.fromJust (M.lookup 19 updatedMap) -- 99
 
@@ -31,3 +30,27 @@ data Case
 
 case1 :: Case
 case1 = Case {description = "456 Oak Ave", input = 42, expected = True}
+
+handleMaybe :: Maybe a -> b -> (a -> b) -> b
+handleMaybe maybeValue defaultValue function =
+  case maybeValue of
+    Nothing -> defaultValue
+    Just value -> function value
+
+-- goodResult :: Int
+-- goodResult = handleMaybe (Just 42) 99 (+2)
+
+-- badResult :: Int
+-- badResult = handleMaybe Nothing 99 (+2)
+
+goodResult :: Int
+goodResult = handleMaybe lookupResult1 99 (+ 2)
+
+badResult :: Int
+badResult = handleMaybe lookupResult2 99 (+ 2)
+
+-- fromMaybe :: a -> Maybe a -> a
+-- Example:
+vvv :: String
+vvv = DM.fromMaybe "Hi" (Just "Hello") -- returns "Hello"
+-- fromMaybe "Hi" Nothing        -- returns "Hi"
