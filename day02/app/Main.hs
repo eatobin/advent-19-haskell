a5dc6bf4c4036828c7d9ca859f5eb402059fb92e
Eric Tobin <eatobin@gmail.com>
2026-02-05 21:59:59 -0700

    progress

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -1,12 +1,13 @@
 module Main (main) where
 
-import Lib (case1, lookupResult1, lookupResult2, lookupResult3, updatedMap)
+import Lib ()
 
 main :: IO ()
 main =
   do
-    print lookupResult1
-    print lookupResult2
-    print updatedMap
-    print lookupResult3
-    print case1
+    -- print lookupResult1
+    -- print lookupResult2
+    -- print updatedMap
+    -- print lookupResult3
+    -- print case1
+    print "hi"
--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -1,56 +1,74 @@
-module Lib (lookupResult1, lookupResult2, updatedMap, lookupResult3, case1, goodResult, badResult, vvv) where
+module Lib () where
+
+-- module Lib (lookupResult1, lookupResult2, updatedMap, lookupResult3, case1, goodResult, badResult, vvv) where
 
 import qualified Data.Map as M
 import qualified Data.Maybe as DM
 
-hasRedHat :: M.Map Int Int
-hasRedHat = M.fromList [(0, 0), (1, 11), (2, 22)]
+type Memory = M.Map Int Int
 
--- Look up an existing key
-lookupResult1 :: Maybe Int
-lookupResult1 = M.lookup 0 hasRedHat -- Just 0
-
--- Look up a non-existing key
-lookupResult2 :: Maybe Int
-lookupResult2 = M.lookup 19 hasRedHat -- Nothing
-
-updatedMap :: M.Map Int Int
-updatedMap = M.insert 19 99 hasRedHat
-
-lookupResult3 :: Int
-lookupResult3 = DM.fromJust (M.lookup 19 updatedMap) -- 99
-
-data Case
-  = Case
-  { description :: String,
-    input :: Integer,
-    expected :: Bool
+data IntCode
+  = IntCode
+  { pointer :: Int,
+    memory :: Memory
   }
   deriving (Show)
 
-case1 :: Case
-case1 = Case {description = "456 Oak Ave", input = 42, expected = True}
+keys :: [Int]
+keys = [0 .. 50]
 
-handleMaybe :: Maybe a -> b -> (a -> b) -> b
-handleMaybe maybeValue defaultValue function =
-  case maybeValue of
-    Nothing -> defaultValue
-    Just value -> function value
+values :: [Int]
+values = [0, 11, 22, 33]
+
+memoryAsList :: [(Int, Int)]
+memoryAsList = zip keys values
+
+intCode :: IntCode
+intCode = IntCode {pointer = 2, memory = M.fromList memoryAsList}
+
+-- memory :: Memory
+-- memory = M.fromList memoryAsList
+
+-- hasRedHat :: M.Map Int Int
+-- hasRedHat = M.fromList [(0, 0), (1, 11), (2, 22)]
+
+-- Look up an existing key
+-- lookupResult1 :: Maybe Int
+-- lookupResult1 = M.lookup 3 intCode.memory -- Just 0
+
+-- -- Look up a non-existing key
+-- lookupResult2 :: Maybe Int
+-- lookupResult2 = M.lookup 19 memory -- Nothing
+
+-- updatedMap :: M.Map Int Int
+-- updatedMap = M.insert 19 99 memory
+
+-- lookupResult3 :: Int
+-- lookupResult3 = DM.fromJust (M.lookup 19 updatedMap) -- 99
+
+-- case1 :: Case
+-- case1 = Case {description = "456 Oak Ave", input = 42, expected = True}
+
+-- handleMaybe :: Maybe a -> b -> (a -> b) -> b
+-- handleMaybe maybeValue defaultValue function =
+--   case maybeValue of
+--     Nothing -> defaultValue
+--     Just value -> function value
+
+-- -- goodResult :: Int
+-- -- goodResult = handleMaybe (Just 42) 99 (+2)
+
+-- -- badResult :: Int
+-- -- badResult = handleMaybe Nothing 99 (+2)
 
 -- goodResult :: Int
--- goodResult = handleMaybe (Just 42) 99 (+2)
+-- goodResult = handleMaybe lookupResult1 99 (+ 2)
 
 -- badResult :: Int
--- badResult = handleMaybe Nothing 99 (+2)
+-- badResult = handleMaybe lookupResult2 99 (+ 2)
 
-goodResult :: Int
-goodResult = handleMaybe lookupResult1 99 (+ 2)
-
-badResult :: Int
-badResult = handleMaybe lookupResult2 99 (+ 2)
-
--- fromMaybe :: a -> Maybe a -> a
--- Example:
-vvv :: String
-vvv = DM.fromMaybe "Hi" (Just "Hello") -- returns "Hello"
--- fromMaybe "Hi" Nothing        -- returns "Hi"
+-- -- fromMaybe :: a -> Maybe a -> a
+-- -- Example:
+-- vvv :: String
+-- vvv = DM.fromMaybe "Hi" (Just "Hello") -- returns "Hello"
+-- -- fromMaybe "Hi" Nothing        -- returns "Hi"
