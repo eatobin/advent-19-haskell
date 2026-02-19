f209ebbae56ccafc66bb07a62ef4286fd0877508
Eric Tobin <eatobin@gmail.com>
2026-02-18 17:22:28 -0700

    in process

--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -32,3 +32,30 @@ makeIntcode pointerParam memoryAsCSVString =
 lookUpFromMemory :: IntCodeStruct -> Pointer -> Int
 lookUpFromMemory intCode pointerParam =
   (memory intCode) IM.! pointerParam
+
+opCode :: IntCodeStruct -> IntCodeStruct
+opCode intCode = case action of
+  1 -> IM.adjust succ 0 (memory intCode)
+  _ -> intCode
+  where
+    action = memory intCode IM.! pointer intCode
+
+-- address1 = memory intCode IM.! pointer intCode + 1
+-- address2 = memory intCode IM.! pointer intCode + 2
+-- address3 = memory intCode IM.! pointer intCode + 3
+
+-- adjust :: (a -> a) -> Key -> IntMap a -> IntMap a
+
+-- cylinder :: (RealFloat a) => a -> a -> a
+-- cylinder r h =
+--   sideArea + 2 * topArea
+--   where
+--     sideArea = 2 * pi * r * h
+--     topArea = pi * r ^ 2
+
+-- describeNumber :: Int -> String
+-- describeNumber x = case x of
+--   0 -> "The number is zero."
+--   1 -> "The number is one."
+--   n | n < 0 -> "The number is negative." -- Guards can be used within case alternatives
+--   _ -> "The number is some other positive integer."
