47edecdc7d581f83d8e0b9b70198f37b10839f1e
Eric Tobin <eatobin@gmail.com>
2026-03-09 16:07:14 -0700

    more instruction tests

--- day02/test/Spec.hs
+++ day02/test/Spec.hs
@@ -11,8 +11,12 @@ main = hspec $ do
       (78 :: Int) `shouldBe` (78 :: Int)
 
   describe "\nInstruction Tests" $ do
-    it "make an Instruction" $ do
-      makeInstruction 9 `shouldBe` Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 9)]
+    it "make an Instruction 1" $ do
+      makeInstruction 6 `shouldBe` Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 6)]
+    it "make an Instruction 3" $ do
+      makeInstruction 456 `shouldBe` Map.fromList [('a', 0), ('b', 0), ('c', 4), ('d', 5), ('e', 6)]
+    it "make an Instruction 5" $ do
+      makeInstruction 23456 `shouldBe` Map.fromList [('a', 2), ('b', 3), ('c', 4), ('d', 5), ('e', 6)]
 
   describe "\nIntCodeStruct Tests" $ do
     let memoryAsCSVString = "10,11,1"
