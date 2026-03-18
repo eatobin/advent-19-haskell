4c2f15f51e55924326aa03d0e2dbe9a27b236767
Eric Tobin <eatobin@gmail.com>
2026-03-18 12:10:00 -0700

    spec cleanup

--- day02/test/Spec.hs
+++ day02/test/Spec.hs
@@ -6,17 +6,23 @@ import Test.Hspec (anyErrorCall, describe, hspec, it, shouldBe, shouldThrow)
 
 main :: IO ()
 main = hspec $ do
+  let instruction1 = Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 6 :: Int)]
+  let instruction3 = Map.fromList [('a', 0), ('b', 0), ('c', 4), ('d', 5), ('e', 6 :: Int)]
+  let instruction5 = Map.fromList [('a', 2), ('b', 3), ('c', 4), ('d', 5), ('e', 6 :: Int)]
+  
+
+
   describe "\nJust test if tests work" $ do
     it "a test all by itself" $ do
       (78 :: Int) `shouldBe` (78 :: Int)
 
   describe "\nInstruction Tests" $ do
     it "make an Instruction 1" $ do
-      makeInstruction 6 `shouldBe` Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 6 :: Int)]
+      makeInstruction 6 `shouldBe` instruction1
     it "make an Instruction 3" $ do
-      makeInstruction 456 `shouldBe` Map.fromList [('a', 0), ('b', 0), ('c', 4), ('d', 5), ('e', 6 :: Int)]
+      makeInstruction 456 `shouldBe` instruction3
     it "make an Instruction 5" $ do
-      makeInstruction 23456 `shouldBe` Map.fromList [('a', 2), ('b', 3), ('c', 4), ('d', 5), ('e', 6 :: Int)]
+      makeInstruction 23456 `shouldBe` instruction5
 
   describe "\nIntCodeStruct Tests" $ do
     let memoryAsCSVString = "10,11,1"
