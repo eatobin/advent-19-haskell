221887ba5a39c4319475cf5fcbf4e16dead2352b
Eric Tobin <eatobin@gmail.com>
2026-06-01 21:22:48 -0700

    add state

--- day07/app/Main.hs
+++ day07/app/Main.hs
@@ -2,17 +2,17 @@
 
 import Lens.Micro.Platform (makeLenses, (%~), (&), (^.))
 
-newtype Address = Address {_city :: String} deriving (Show)
+data Address = Address {_city :: String, _state :: String} deriving (Show)
 
 data User = User {_name :: String, _address :: Address} deriving (Show)
 
 -- Automatically generates _city and _name lenses
 makeLenses ''Address
-makeLenses ''User
+makeLenses 'User
 
 main :: IO ()
 main = do
-  let user = User "Alice" (Address "Phoenix")
+  let user = User "Alice" (Address "Phoenix" "AZ")
 
   -- 1. Get a nested field
   print $ user ^. address . city -- Output: "Phoenix"
@@ -20,4 +20,5 @@ main = do
   -- 2. Modify a field
   print $ user & name %~ (++ " Smith") -- Output: User "Alice Smith" ...
 
--- Google haskell microlens platform tutorial
+  -- Google haskell microlens platform tutorial
+  print $ user ^. address . state
