f74d2d0afe92f1906401e96cfd1fd48cf568040a
Eric Tobin <eatobin@gmail.com>
2026-07-15 20:43:58 -0700

    clean

--- day02/src/Lib.hs
+++ day02/src/Lib.hs
@@ -129,16 +129,6 @@ multiply instruction intcode =
                  ]
     }
 
--- runSessionAdd :: IntCodeState ()
--- runSessionAdd = do
---   let instruction = Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 6 :: Int)]
---   add instruction
-
--- runSessionMult :: IntCodeState ()
--- runSessionMult = do
---   let instruction = Map.fromList [('a', 0), ('b', 0), ('c', 0), ('d', 0), ('e', 6 :: Int)]
---   multiply instruction
-
 runOpCode :: IntCodeState ()
 runOpCode = do
   currentState <- get
@@ -152,69 +142,3 @@ runOpCode = do
       runOpCode
     9 -> do put currentState
     _ -> error "Instruction is not valid"
-
--- import Control.Monad.State
-
--- incrementState :: State Int ()
--- incrementState = modify (+1)
-
--- -- Running the state: initial state is 10
--- -- execState returns only the final state
--- main :: IO ()
--- main = print $ execState incrementState 10  -- Output: 11
-
--- mainX :: IO ()
--- mainX = do
---   let thisMemoryAddMult = Vec.fromList [0, 2, 1, 0]
---   let initialIntCode = IntCode {pointer = 0, memory = thisMemoryAddMult}
---   let (results, finalState) = runState runSessionAdd initialIntCode
---   let xxx = evalState runSessionAdd initialIntCode
---   let yyy = execState runSessionAdd initialIntCode
---   print results
---   putStrLn $ "Final Add: " ++ show finalState
---   print xxx
---   print yyy
---   let (resultsM, finalStateM) = runState runSessionMult initialIntCode
---   print resultsM
---   putStrLn $ "Final Mult: " ++ show finalStateM
-
--- -- 3. Sequence multiple stateful operations inside a do-block
--- gameSession :: State InventoryState [String]
--- gameSession = do
---   msg1 <- addItem "Iron Sword" 4.5
---   msg2 <- addItem "Healing Potion" 1.0
---   msg3 <- addItem "Heavy Gold Chest" 8.0 -- This one should fail due to weight limits
---   return [msg1, msg2, msg3]
-
--- runOpCode :: IntCodeState -> IntCodeState
--- runOpCode intCode =
---   case instruction Map.! 'e' of
---     1 -> runOpCode (add instruction intCode)
---     2 -> runOpCode (multiply instruction intCode)
---     9 -> intCode
---     _ -> error "Instruction is not valid"
---   where
---     instruction = makeInstruction (memory intCode Vec.! pointer intCode)
-
--- -- The state is an Int (the current countdown number).
--- -- The result of the computation is a list of Strings (the emitted outputs).
--- type CountdownState = State Int [String]
-
--- countdownStep :: CountdownState
--- countdownStep = do
---   n <- get -- Get the current state
---   if n <= 0
---     then do
---       return ["Blast off!"]
---     else do
---       put (n - 1) -- Update state (decrement)
---       let msg = "Counting down: " ++ show n
---       rest <- countdownStep -- Recursively call next step
---       return (msg : rest) -- Cons the current output to the rest
-
--- main :: IO ()
--- main = do
---   let (outputs, finalState) = runState countdownStep 5
---   print outputs
---   -- mapM_ putStrLn outputs
---   putStrLn $ "Final state: " ++ show finalState
