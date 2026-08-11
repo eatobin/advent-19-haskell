5a73b773948fbea6112a53d8cda12d5ec4f7a02c
Eric Tobin <eatobin@gmail.com>
2026-08-11 10:45:14 -0700

    works great!!

--- day02/app/Main.hs
+++ day02/app/Main.hs
@@ -5,38 +5,38 @@ import qualified Data.Vector as Vec
 import Lib (IntCode (..), Memory, makeMemory, runOpCode, updatedMemory)
 import Text.Printf (printf)
 
-makeThisMemory :: Memory
-makeThisMemory =
-  let memoryAsCSVString = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0"
-   in makeMemory memoryAsCSVString
+makeThisMemory :: String -> Memory
+makeThisMemory = makeMemory
 
-candidatePairs :: [(Int, Int)]
-candidatePairs =
+makeCandidatePairs :: [(Int, Int)]
+makeCandidatePairs =
   [(noun, verb) | noun <- [0 .. (99 :: Int)], verb <- [0 .. (99 :: Int)]]
 
-runAcandidatePair :: (Int, Int) -> ((Int, Int), Int)
-runAcandidatePair candidatePair =
+runAcandidatePair :: Memory -> (Int, Int) -> ((Int, Int), Int)
+runAcandidatePair thisMemory candidatePair =
   let candidateIntCode =
-        execState runOpCode (IntCode {pointer = 0, memory = uncurry updatedMemory candidatePair makeThisMemory, actions = []})
+        execState runOpCode (IntCode {pointer = 0, memory = uncurry updatedMemory candidatePair thisMemory, actions = []})
    in (candidatePair, memory candidateIntCode Vec.! 0)
 
-mapOverPairs :: [((Int, Int), Int)]
-mapOverPairs =
-  map runAcandidatePair candidatePairs
+mapOverPairs :: [(Int, Int)] -> Memory -> [((Int, Int), Int)]
+mapOverPairs pairs thisMemory =
+  map (runAcandidatePair thisMemory) pairs
 
 winnerIs :: ((Int, Int), Int) -> Bool
 winnerIs candidate =
   let ((_, _), calculation) = candidate
    in calculation == 19690720
 
-findWinner :: ((Int, Int), Int)
-findWinner = head (filter winnerIs mapOverPairs)
+findWinner :: [(Int, Int)] -> Memory -> ((Int, Int), Int)
+findWinner pairs thisMemory = head (filter winnerIs (mapOverPairs pairs thisMemory))
 
 main :: IO ()
 main =
   do
     -- part A
-    let initialStateA = IntCode {pointer = 0, memory = updatedMemory 12 2 makeThisMemory, actions = []}
+    let memoryAsCSVString = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,9,19,23,2,13,23,27,1,6,27,31,2,6,31,35,2,13,35,39,1,39,10,43,2,43,13,47,1,9,47,51,1,51,13,55,1,55,13,59,2,59,13,63,1,63,6,67,2,6,67,71,1,5,71,75,2,6,75,79,1,5,79,83,2,83,6,87,1,5,87,91,1,6,91,95,2,95,6,99,1,5,99,103,1,6,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0"
+    let theMainMemory = makeThisMemory memoryAsCSVString
+    let initialStateA = IntCode {pointer = 0, memory = updatedMemory 12 2 theMainMemory, actions = []}
 
     let finalStateA = execState runOpCode initialStateA
     let answer1 = memory finalStateA Vec.! 0
@@ -45,12 +45,13 @@ main =
     print $ reverse (actions finalStateA)
 
     -- part B
-    let ((noun, verb), _) = findWinner
+    let pairs = makeCandidatePairs
+    let ((noun, verb), _) = findWinner pairs theMainMemory
     let answer2 = (100 * noun) + verb
 
     printf "\nPart B answer = %u. Correct = 8226.\n" (answer2 :: Int)
 
-    let initialStateB = IntCode {pointer = 0, memory = updatedMemory noun verb makeThisMemory, actions = []}
+    let initialStateB = IntCode {pointer = 0, memory = updatedMemory noun verb theMainMemory, actions = []}
     let finalStateB = execState runOpCode initialStateB
     print $ reverse (actions finalStateB)
     putStrLn ""
